c13d31a0bc01233c944eef21b3e23f5e437740aa
SettingDust <settingdust@gmail.com>
2025-02-05 21:45:58 +0800

    fix(recipe): random block ticking not working if block implementation overriding isRandomlyTicking
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- src/main/java/snownee/lychee/mixin/ReloadableServerResourcesMixin.java
+++ src/main/java/snownee/lychee/mixin/ReloadableServerResourcesMixin.java
@@ -23,19 +23,33 @@ import snownee.lychee.fragment.Fragments;
 @Mixin(ReloadableServerResources.class)
 public class ReloadableServerResourcesMixin {
 
-	// because tags are loaded after recipes, we bake cache here
-	@Inject(at = @At("TAIL"), method = "updateRegistryTags(Lnet/minecraft/core/RegistryAccess;)V")
-	private void lychee_updateRegistryTags(CallbackInfo ci) {
-		RecipeTypes.buildCache();
-	}
-
 	@Inject(
 			at = @At(
-					value = "INVOKE", target = "Lnet/minecraftforge/event/ForgeEventFactory;onResourceReload(Lnet/minecraft/server/ReloadableServerResources;Lnet/minecraft/core/RegistryAccess;)Ljava/util/List;", remap = false
+					value = "INVOKE",
+					target = "Lnet/minecraftforge/event/ForgeEventFactory;onResourceReload(Lnet/minecraft/server/ReloadableServerResources;Lnet/minecraft/core/RegistryAccess;)Ljava/util/List;",
+					remap = false
 			), method = "loadResources", locals = LocalCapture.CAPTURE_FAILEXCEPTION
 	)
-	private static void lychee_loadResources(ResourceManager p_248588_, RegistryAccess.Frozen p_251163_, FeatureFlagSet p_250212_, Commands.CommandSelection p_249301_, int p_251126_, Executor p_249136_, Executor p_249601_, CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> cir, ReloadableServerResources reloadableserverresources, List<PreparableReloadListener> listeners) {
+	private static void lychee_loadResources(
+			ResourceManager p_248588_,
+			RegistryAccess.Frozen p_251163_,
+			FeatureFlagSet p_250212_,
+			Commands.CommandSelection p_249301_,
+			int p_251126_,
+			Executor p_249136_,
+			Executor p_249601_,
+			CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> cir,
+			ReloadableServerResources reloadableserverresources,
+			List<PreparableReloadListener> listeners) {
 		listeners.add(0, Fragments.INSTANCE);
+	}
+
+	// because tags are loaded after recipes, we bake cache here
+	@Inject(
+			method = "updateRegistryTags(Lnet/minecraft/core/RegistryAccess;)V",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Blocks;rebuildCache()V"))
+	private void lychee_updateRegistryTags(CallbackInfo ci) {
+		RecipeTypes.buildCache();
 	}
 
 }
